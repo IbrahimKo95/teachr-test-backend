33bc6114b1a9915ff7b5af19d2e8d9d5ded23a8d
IbrahimKo95 <ibrahkonate95@gmail.com>
2024-12-01 17:59:07 +0100

    feat: DELETE and PUT Routes for Products

--- src/Controller/API/ProductController.php
+++ src/Controller/API/ProductController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Requirement\Requirement;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
@@ -45,6 +46,30 @@ class ProductController extends AbstractController
         $em->persist($product);
         $em->flush();
         return $this->json($product, 201, [], ['groups' => 'products']);
+    }
+
+    #[Route('/api/products/{id}', requirements: ['id' => Requirement::DIGITS], methods: ['DELETE'])]
+    public function delete(Product $product, EntityManagerInterface $em)
+    {
+        $em->remove($product);
+        $em->flush();
+        return new JsonResponse(null, 204);
+    }
+
+    #[Route('/api/products/{id}', requirements: ['id' => Requirement::DIGITS], methods: ['PUT'])]
+    public function update(Request $request, Product $currentBook, SerializerInterface $serializer, EntityManagerInterface $em, CategoryRepository $categoryRepository, ValidatorInterface $validator)
+    {
+        $updateProduct = $serializer->deserialize($request->getContent(), Product::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $currentBook]);
+
+        $errors = $validator->validate($updateProduct);
+        if (count($errors) > 0) {
+            return new JsonResponse($serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
+        }
+        $category = $categoryRepository->find($request->toArray()['category_id']);
+        $updateProduct->setCategory($category);
+        $em->persist($updateProduct);
+        $em->flush();
+        return $this->json($updateProduct, 201, [], ['groups' => 'products']);
     }
 
 }
